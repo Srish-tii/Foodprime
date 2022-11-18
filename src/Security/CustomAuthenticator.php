a84a09717e99fb33750f59099575ab0c94e9fca0
Srishti Lodha <srishtilodha2000@gmail.com>
2022-11-18 12:54:58 +0100

    added registration feature

--- src/Security/CustomAuthenticator.php
+++ src/Security/CustomAuthenticator.php
@@ -48,7 +48,7 @@ class CustomAuthenticator extends AbstractLoginFormAuthenticator
 
         // For example:
         // return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('dishes.edit'));
     }
 
     protected function getLoginUrl(Request $request): string
