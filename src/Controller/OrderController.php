12cf2336bee427c8f907d16e2796c59c8eacb1c8
Srishti Lodha <srishtilodha2000@gmail.com>
2022-11-18 14:54:45 +0100

    added ordering option

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -2,17 +2,67 @@
 
 namespace App\Controller;
 
+use App\Entity\Dishes;
+use App\Entity\Order;
+use App\Repository\OrderRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\Routing\Annotation\Route;
 
 class OrderController extends AbstractController
 {
     #[Route('/order', name: 'app_order')]
-    public function index(): Response
+    public function index(OrderRepository $orderRepository): Response
     {
+
+        $order = $orderRepository->findBy(
+            ['new_table'=>'new_table1']
+        );
         return $this->render('order/index.html.twig', [
-            'controller_name' => 'OrderController',
+            'orders' => $order,
         ]);
+    }
+    #[Route('/new_order/{id}', name: 'new_order')]
+    public function new_order(Dishes $dishes, ManagerRegistry $doctrine){
+        $order=new Order();
+        $order->setNewTable("new_table");
+        $order->setName($dishes->getName());
+        // $order->setOrderNumber($dishes->getOrderNumber());
+        $order->setPrice($dishes->getprice());
+        $order->setStatus("open");
+
+        $em = $doctrine -> getManager();
+        $em->persist($order);
+        $em->flush();
+
+        $this->addFlash('Order', $order->getName().'has been added to the cart!');
+        return $this->redirect($this->generateUrl('app_menu'));
+        
+    }
+
+    
+    #[Route('/status/{id},{status}', name: 'status')]
+    public function status($id, $status, ManagerRegistry $doctrine)
+    {
+        
+        $em = $doctrine -> getManager();
+        $order = $em->getRepository(Order::class)->find($id);
+
+        $order->setStatus($status);
+        $em->flush();
+
+        return $this->redirect($this->generateUrl('order'));
+    }
+
+    #[Route('/delete/{id}', name: 'delete')]
+    public function entfernen($id, OrderRepository $or, ManagerRegistry $doctrine)
+    {
+        $em = $doctrine -> getManager();
+        $order = $or->find($id);
+        $em->remove($order);
+        $em->flush();
+
+        return $this->redirect($this->generateUrl('order'));
     }
 }
---
+++ src/Entity/Order.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\OrderRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: OrderRepository::class)]
+#[ORM\Table(name: '`order`')]
+class Order
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $new_table = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $order_number = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $name = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?float $price = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $status = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNewTable(): ?string
+    {
+        return $this->new_table;
+    }
+
+    public function setNewTable(?string $new_table): self
+    {
+        $this->new_table = $new_table;
+
+        return $this;
+    }
+
+    public function getOrderNumber(): ?string
+    {
+        return $this->order_number;
+    }
+
+    public function setOrderNumber(?string $order_number): self
+    {
+        $this->order_number = $order_number;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(?float $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(?string $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+}
